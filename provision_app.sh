33c524fe9c73a6cda785df640b642990a982320e
kaiwolff <kai.wolff.92@gmail.com>
2021-11-18 10:35:25 +0000

    updated provisioning script, added nginx default file

--- provision_app.sh
+++ provision_app.sh
@@ -20,19 +20,20 @@ sudo apt-get upgrade -y
 #should now have all prerequisites installed, time to change directory to the app directory and run the npm commands to install and run the app
 #add new environment variable to bashrc and run source to reload it
 
-sudo echo 'export DB_HOST="mongodb://192.168.10.150:27017/posts"' >> .bashrc
-source ~/.bashrc
+#sudo echo 'export DB_HOST="mongodb://192.168.10.150:27017/posts"' >> .bashrc
+#temporarily commented this part out until new db Ip available
+#source ~/.bashrc
 
 #replace default of nginx. this hsoudl have been provisioned into /home/environment by Vagrant
 sudo rm /etc/nginx/sites-available/default
-sudo cp /home/environment/default /etc/nginx/sites-available/default
+sudo cp ~/DevOpsBootcamp_Jenkins/default /etc/nginx/sites-available/default
 
 sudo service nginx restart
 #sudo systemctl enable nginx
 
 #node app/app/seeds/seed.js
 
-cd /home/app
+cd ~/DevOpsBootcamp_Jenkins/app
 node seeds/seed.js
 sudo npm install
 sudo npm install express
